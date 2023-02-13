65221485b4518ae07c96c62416c49678000a73dd
Nimesh Nuwanga <nuwangasenevirathna01@gmail.com>
2023-02-13 13:21:04 +0530

    Add back-end code to repository
    This commit adds the initial implementation of the back-end code using Spring Boot. The code includes the setup of the basic project structure, configuration of dependencies, and implementation of RESTful API endpoints.

---
+++ Back-End/src/main/java/com/Infolinx/cookie_consent_management_system/CookieConsentManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Infolinx.cookie_consent_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookieConsentManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookieConsentManagementSystemApplication.class, args);
+	}
+
+}
